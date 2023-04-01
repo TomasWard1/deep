4e63986a1405aefbb2d24a1c497e31ba47fcd75f
santi-nihany <santi.nihany@gmail.com>
2023-04-01 01:06:16 -0300

    bookNft, mint.js, move-blocks.js added

---
+++ hardhat/deploy/02-deploy-booknft.js
@@ -0,0 +1,26 @@
+const {
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config");
+const { network } = require("hardhat");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+
+    let args = [];
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS;
+
+    log("---------------------------------------");
+    const bookNft = await deploy("BookNft", {
+        from: deployer,
+        log: true,
+        args: args,
+        waitConfirmations: waitBlockConfirmations,
+    });
+    log("----------------------------------------------------");
+};
+
+module.exports.tags = ["all", "booknft"];
