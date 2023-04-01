2fcdc06407596d2f69f10e31a30e0027c6999cd4
Luz Alba Posse <luzalbaposse@gmail.com>
2023-03-31 22:34:57 -0300

    Initial commit
    Este es el set up de flutter para arrancar ✅

---
+++ deep_luz/README.md
@@ -0,0 +1,3 @@
+# deep_luz
+
+A new Flutter project.
---
+++ deep_luz/android/app/src/main/kotlin/com/example/deep_luz/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.deep_luz
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
